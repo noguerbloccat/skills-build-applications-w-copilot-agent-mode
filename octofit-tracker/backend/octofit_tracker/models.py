84f219817056c9ab490dabb7d0b0fdd53746e3dc
noguerbloccat <noguer.bloc.cat@gmail.com>
2026-01-12 09:03:19 +0000

    Add initial Django project structure and models for OctoFit Tracker

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,36 @@
+from djongo import models
+
+class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='activities')
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()  # minutes
+    date = models.DateField()
+
+class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    suggested_for = models.ManyToManyField(UserProfile, related_name='suggested_workouts')
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Leaderboard'
